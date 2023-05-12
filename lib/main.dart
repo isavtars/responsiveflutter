da5ada43768306a53c41c245a38d0d8e8d35c135
isavtars <113200845+isavtars@users.noreply.github.com>
2023-05-12 14:36:55 +0545

    mediquery

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [LayoutBuilder].
 /// //Creating a responsive Flutter app
+/// A widget that attempts to size the child to a specific aspect ratio.
 
 void main() => runApp(const LayoutBuilderExampleApp());
 
@@ -12,57 +12,49 @@ class LayoutBuilderExampleApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LayoutBuilderExample(),
+      home: MyApp(),
     );
   }
 }
 
-class LayoutBuilderExample extends StatelessWidget {
-  const LayoutBuilderExample({super.key});
+///AspectRatio
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var height, weight;
+    var size = MediaQuery.of(context).size;
+    height = size.height;
+    weight = size.width;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('LayoutBuilder Example')),
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          if (constraints.maxWidth > 600) {
-            return _buildWideContainers();
-          } else {
-            return _buildNormalContainer();
-          }
-        },
-      ),
-    );
-  }
-
-  Widget _buildNormalContainer() {
-    return Center(
-      child: Container(
-        height: 100.0,
-        width: 100.0,
-        color: const Color.fromARGB(255, 92, 244, 54),
-      ),
-    );
-  }
-
-  Widget _buildWideContainers() {
-    return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            height: 100.0,
-            width: 100.0,
-            color: const Color.fromARGB(255, 14, 10, 226),
-          ),
-          Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.yellow,
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("MediaQuerys"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: height * 0.2,
+              width: weight,
+              color: Colors.red,
+            ),
+            Container(
+              height: height * 0.2,
+              width: weight,
+              color: const Color.fromARGB(255, 3, 247, 76),
+            ),
+            Container(
+              height: height * 0.2,
+              width: weight,
+              color: const Color.fromARGB(255, 245, 9, 138),
+            ),
+            Container(
+              height: height * 0.1,
+              width: weight,
+              color: const Color.fromARGB(255, 12, 185, 216),
+            )
+          ],
+        ));
   }
 }
