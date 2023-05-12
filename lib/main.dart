dade2f27ebc8ff0f9b29ecc08739e924ba06179b
isavtars <113200845+isavtars@users.noreply.github.com>
2023-05-12 13:57:38 +0545

    responsive

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [LayoutBuilder].
+/// //Creating a responsive Flutter app
+
+void main() => runApp(const LayoutBuilderExampleApp());
+
+class LayoutBuilderExampleApp extends StatelessWidget {
+  const LayoutBuilderExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LayoutBuilderExample(),
+    );
+  }
+}
+
+class LayoutBuilderExample extends StatelessWidget {
+  const LayoutBuilderExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('LayoutBuilder Example')),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth > 600) {
+            return _buildWideContainers();
+          } else {
+            return _buildNormalContainer();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildNormalContainer() {
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: const Color.fromARGB(255, 92, 244, 54),
+      ),
+    );
+  }
+
+  Widget _buildWideContainers() {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            height: 100.0,
+            width: 100.0,
+            color: const Color.fromARGB(255, 14, 10, 226),
+          ),
+          Container(
+            height: 100.0,
+            width: 100.0,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+    );
+  }
+}
